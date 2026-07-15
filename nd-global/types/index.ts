e547e6cfad6c4a90f0ebfa4f3b23d7fe1f071c86
marketingndglobal-coder <marketing.ndglobal@gmail.com>
2026-07-15 11:23:16 +0000

    Initial website

---
+++ nd-global/types/index.ts
@@ -0,0 +1,62 @@
+export interface Product {
+  id: string;
+  name: string;
+  slug: string;
+  category: string;
+  tagline: string;
+    homeText: string;
+  description: string;
+  finish: string[];
+  sizes: string[];
+  uses: string[];
+  image: string;
+  
+  homeImage: string;   // 👈 yeh naya add karo
+
+  gallery?: string[]; // additional images
+  color: string;
+}
+
+
+
+// export type Category = "floor" | "wall" | "table";
+
+// export interface RoomState {
+//   floor: number;
+//   wall: number;
+//   table: number;
+// }
+
+// export interface SwatchOption {
+//   label: string;
+//   color: string;
+// }
+
+// export interface FloorOption extends SwatchOption {
+//   lineColor: string;
+// }
+
+// export interface TableOption extends SwatchOption {
+//   legColor: string;
+// }
+
+// export interface Product {
+//   id: string;
+//   name: string;
+//   slug: string;
+//   category: string;
+//   tagline: string;
+//   description: string;
+//   finish: string[];
+//   sizes: string[];
+//   uses: string[];
+//   image: string;
+  
+//   gallery?: string[]; // additional images
+//   color: string;
+// }
+
+// export interface NavLink {
+//   label: string;
+//   href: string;
+// }
